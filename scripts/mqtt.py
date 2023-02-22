7997793b18878f5045b9b6e92c0a344164c67145
Damien Allen <dev@dallen.co>
2023-02-22 23:49:04 +0100

    MQTT broker setup

---
+++ scripts/mqtt.py
@@ -0,0 +1,15 @@
+import asyncio
+
+from asyncio_mqtt import Client
+
+
+async def main():
+    async with Client("localhost") as client:
+        async with client.messages() as messages:
+            await client.subscribe("tele/tasmota_133D41/SENSOR")
+            async for message in messages:
+                print(message.payload)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
