77fa482674a8721b50829c490f204041bd40480f
Damien Allen <dev@dallen.co>
2023-02-26 23:19:33 +0100

    Expose df via API

--- server/app/main.py
+++ server/app/main.py
@@ -1,5 +1,10 @@
+from datetime import datetime
+
+import pandas as pd
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
+
+from server import DATA_DIR
 
 app = FastAPI()
 
@@ -16,4 +21,24 @@ app.add_middleware(
 
 @app.get("/sensors/")
 def sensors(request: Request):
-    return {"Hello": "you"}
+    x, y = load_df(datetime.now())
+    return [
+        {
+            "type": "scatter",
+            "mode": "lines",
+            "name": "Temperature (degC)",
+            "x": x,
+            "y": y,
+            "line": {"color": "#7F7F7F"},
+        }
+    ]
+
+
+def load_df(date: datetime) -> pd.DataFrame:
+    record_path = DATA_DIR / str(date.year) / str(date.month) / f"{date.day}.xz"
+
+    if not record_path.exists():
+        return [], []
+
+    df = pd.read_pickle(record_path)
+    return list(str(d) for d in df.index.values), df["temperature"].tolist()
