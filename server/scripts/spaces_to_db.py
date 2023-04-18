36bdae6def68b5d4800a29b38f703a14b6a62d11
Damien Allen <dev@dallen.co>
2023-04-18 23:14:39 +0100

    Calculate brightness for 2020 files

--- server/scripts/spaces_to_db.py
+++ server/scripts/spaces_to_db.py
@@ -1,18 +1,21 @@
 from influxdb_client import Point
-import os
+from tempfile import NamedTemporaryFile
 import subprocess
+import httpx
+
 
 from server.hub.spaces import client
-from server.hub.influx import bucket_name
+from server.hub.influx import write_api, bucket_name
 from datetime import datetime
 from tqdm import tqdm
 
+
 ORIGIN_ENDPOINT = "https://growcam.ams3.digitaloceanspaces.com"
+DARK_THRESHOLD = 0.1
 
 
 def run():
-    bucket_name = "growcam"
-    bucket_objs = client.list_objects(Bucket=bucket_name)["Contents"]
+    bucket_objs = client.list_objects(Bucket="growcam")["Contents"]
 
     pbar = tqdm(bucket_objs)
     for obj in pbar:
@@ -21,20 +24,24 @@ def run():
 
         pbar.set_description(key)
 
-        # sp = subprocess.run(["vips", "avg", file_path], capture_output=True, text=True)
+        with NamedTemporaryFile() as tmp:
+            r = httpx.get(url)
+            tmp.write(r.content)
 
-        # brightness = round(float(sp.stdout.replace("\n", "")) / 255, 2)
+            sp = subprocess.run(
+                ["vips", "avg", tmp.name], capture_output=True, text=True
+            )
+            brightness = round(float(sp.stdout.replace("\n", "")) / 255, 2)
+            dark = brightness < DARK_THRESHOLD
 
-        # file_name = file_path.name
-        # url = f"/media/captures/2023/{file_name}"
-        # p = (
-        #     Point("photo")
-        #     .field("brightness", brightness)
-        #     .tag("url", url)
-        #     .time(int(datetime.strptime(file_name, "%Y%m%d_%H%M%S.jpg").timestamp()))
-        # )
-        # write_api.write(bucket=bucket_name, record=p)
-        # print(p)
+        p = (
+            Point("photo")
+            .field("brightness", brightness)
+            .field("url", url)
+            .tag("dark", dark)
+            .time(datetime.strptime(key, "photo_%Y%m%d_%H%M%S.jpg"))
+        )
+        write_api.write(bucket=bucket_name, record=p)
 
 
 if __name__ == "__main__":
